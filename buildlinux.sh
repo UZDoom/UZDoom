bebe794bf104738dc5e1d521a4c44a6182c05541
blockplace1232 <156526592+blockplace1232@users.noreply.github.com>
2025-10-19 14:38:08 +0000

    (hopefully) removed 32-bit compilation

--- buildlinux.sh
+++ buildlinux.sh
@@ -5,7 +5,6 @@ mkdir -pv ~/uzdoom_build
 cd ~/uzdoom_build &&
 git clone https://github.com/UZDoom/uzdoom.git &&
 mkdir -pv uzdoom/build
-a='' && [ "$(uname -m)" = x86_64 ] && a=64
 c="$(lscpu -p | grep -v '#' | sort -u -t , -k 2,4 | wc -l)" ; [ "$c" -eq 0 ] && c=1
 cd ~/uzdoom_build/uzdoom/build &&
 rm -f output_sdl/liboutput_sdl.so &&
