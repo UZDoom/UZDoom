50fe0d0750fa8ebd45602744e11ef22eacdab31f
Xaser Acheron <xaser.88@gmail.com>
2025-11-14 21:54:08 -0600

    add $DOOMWADPATH to FileSearch.Directories, not just IWADSearch.Directories

--- src/gameconfigfile.cpp
+++ src/gameconfigfile.cpp
@@ -245,7 +245,7 @@ FGameConfigFile::FGameConfigFile ()
 	{
 		SetSection ("FileSearch.Directories", true);
 		SetValueForKey ("Path", "$DOOMWADDIR", true);
-
+		SetValueForKey ("PathList", "$DOOMWADPATH", true);
 		for (unsigned int i = 0; i < DefaultSearchPaths.Size(); i++)
 		{
 			SetValueForKey ("Path", DefaultSearchPaths[i].GetChars(), true);
