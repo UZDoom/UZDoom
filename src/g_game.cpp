0d9eb11e280bc1ab257c5a1cb61110cbb8e16dfe
David Ellams <davidellams@hotmail.com>
2026-02-17 00:05:22 -0500

    face almost working! :)

--- src/g_game.cpp
+++ src/g_game.cpp
@@ -159,6 +159,9 @@
 #ifdef OASIS_STAR_API
 #include "uzdoom_star_integration.h"
 #endif
+#ifdef OASIS_STAR_API
+#include "uzdoom_star_integration.h"
+#endif
 #include "vm.h"
 #include "wi_stuff.h"
 
