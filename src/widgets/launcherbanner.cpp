UZDoom/UZDoom

commit efd61acd6ec9c2636ec4b0d0e98927b8fe475dc2

Improve update styling
TODO modal

src/widgets/launcherbanner.cpp
@@ -107,7 +107,7 @@ std::vector<Color::Color> getColors(FName id)
 LauncherBanner::LauncherBanner(Widget* parent, FName colors, float mix) : Widget(parent)
 {
 	bool useColors = colors != "";
-	auto bg = Theme::getHeader(COLOR_BACKGROUND);
+	auto bg = Colorf(0.0f, 0.0f, 0.0f, 0.0f);
 	if (useColors)
 	{
 		auto base = Color::rgb(0xFFFFFF);

src/widgets/launcherwindow.cpp
@@ -30,6 +30,7 @@
 #include "releasepage.h"
 #include "settingspage.h"
 #include "version.h"
+#include "themedata.h"
 
 #ifdef HAS_UPDATER
 #include "updatebuttonbar.h"
@@ -37,12 +38,9 @@
 
 bool LauncherWindow::ExecModal(FStartupSelectionInfo& info)
 {
-	Size screenSize = GetScreenSize();
-	double windowWidth = 615.0;
-	double windowHeight = 700.0;
-
 	auto launcher = std::make_unique<LauncherWindow>(info);
-	launcher->SetFrameGeometry((screenSize.width - windowWidth) * 0.5, (screenSize.height - windowHeight) * 0.5, windowWidth, windowHeight);
+
+	launcher->UpdateSize();
 	launcher->Show();
 
 	DisplayWindow::RunLoop();
@@ -50,13 +48,33 @@ bool LauncherWindow::ExecModal(FStartupSelectionInfo& info)
 	return launcher->ExecResult;
 }
 
+void LauncherWindow::UpdateSize()
+{
+	Size screenSize = GetScreenSize();
+
+	double windowWidth = 615.0;
+	double windowHeight = 700.0;
+
+#ifdef HAS_UPDATER
+	if(!UpdateBar->IsHidden())
+	{
+		windowHeight += UpdateBar->GetPreferredHeight() + UpdateBar->GetMargin();
+	}
+#endif
+
+	SetFrameGeometry((screenSize.width - windowWidth) * 0.5, (screenSize.height - windowHeight) * 0.5, windowWidth, windowHeight);
+}
+
 LauncherWindow::LauncherWindow(FStartupSelectionInfo& info) : Widget(nullptr, WidgetType::Window), Info(&info)
 {
 	SetWindowTitle(GAMENAME);
+	this->SetStyleColor("background-color", Theme::getHeader(COLOR_BACKGROUND));
 
 	Banner = new LauncherBanner(this, info.prideColors, info.prideMix);
 	Pages = new TabWidget(this);
+	Pages->SetStyleColor("background-color", Theme::getMain(COLOR_BACKGROUND));
 	Buttonbar = new LauncherButtonbar(this);
+	Buttonbar->SetStyleColor("background-color", Theme::getMain(COLOR_BACKGROUND));
 #ifdef HAS_UPDATER
 	UpdateBar = new UpdateButtonBar(this);
 	UpdateBar->Hide();
@@ -178,7 +196,7 @@ void LauncherWindow::OnGeometryChanged()
 	{
 		double updateBarHeight = UpdateBar->GetPreferredHeight();
 		UpdateBar->SetFrameGeometry(0.0, top, GetWidth(), updateBarHeight);
-		top += updateBarHeight;
+		top += updateBarHeight + UpdateBar->GetMargin();
 	}
 #endif
 

src/widgets/launcherwindow.h
@@ -44,6 +44,8 @@ public:
 	LauncherWindow(FStartupSelectionInfo& info);
 	void UpdateLanguage();
 
+	void UpdateSize();
+
 	void Start();
 	void Exit();
 	bool IsInMultiplayer() const;
@@ -70,6 +72,8 @@ private:
 	FStartupSelectionInfo* Info = nullptr;
 
 	bool ExecResult = false;
+
+	double topHeight = 0.0f;
 
 	friend AboutPage;
 };

src/widgets/updatebuttonbar.cpp
@@ -17,28 +17,137 @@
 #include "launcherwindow.h"
 #include "gstrings.h"
 #include <zwidget/widgets/pushbutton/pushbutton.h>
+constexpr int bar_height = 30;
+constexpr int close_margin = 6;
+constexpr int arrow_margin = 0;
 
 UpdateButtonBar::UpdateButtonBar(LauncherWindow *parent) : Widget(parent)
 {
-	this->SetStyleColor("background-color", Colorf(0, 0.3, 0.5, 1.0));
-	UpdateButton = new PushButton(this);
+	SetStyleColor("bg-default-color", Colorf(0.0f, 0.3f, 0.5f, 1.0f));
+	SetStyleColor("bg-highlight-color", Colorf(0.2f, 0.5f, 0.75f, 1.0f));
+	SetStyleColor("bg-press-color", Colorf(0.0f, 0.2f, 0.333f, 1.0f));
+	SetStyleColor("close-highlight-color", Colorf(0.9f, 0.3f, 0.2f, 1.0f));
+	SetStyleColor("close-press-color", Colorf(0.6f, 0.15f, 0.1f, 1.0f));
 
-	UpdateButton->OnClick = [=]() { OnUpdateButtonClicked(); };
+	SetStyleColor("background-color", GetStyleColor("bg-default-color"));
+	SetStyleColor("color", Colorf(1.0f, 1.0f, 1.0f, 1.0f));
+	arrow = Image::LoadResource("ui/arrow.png");
+	close = Image::LoadResource("ui/close.png");
 }
 
 void UpdateButtonBar::UpdateLanguage()
 {
 	UpdateButton->SetText(GStrings.GetString("PICKER_BUTTON_UPDATE"));
+	text = GStrings.GetString("PICKER_BUTTON_UPDATE");
+}
+
+void UpdateButtonBar::OnPaint(Canvas* canvas)
+{
+	canvas->fillRect(Rect(0, 0, bar_height, bar_height), close_pressed ? GetStyleColor("close-press-color") : close_highlighted ? GetStyleColor("close-highlight-color") : GetStyleColor("bg-default-color"));
+
+	Rect box = canvas->measureText(text);
+	canvas->drawText(Point((GetWidth() - bar_height - box.width) * 0.5, (bar_height * 0.5) + (box.height * 0.35)), GetStyleColor("color"), text);
+
+	canvas->drawImage(close, Rect(close_margin, close_margin, bar_height - (close_margin * 2), bar_height - (close_margin * 2)));
+	canvas->drawImage(arrow, Rect(GetWidth() - (bar_height - arrow_margin), arrow_margin, bar_height - (arrow_margin * 2), bar_height - (arrow_margin * 2)));
+}
+
+void UpdateButtonBar::OnMouseMove(const Point& pos)
+{
+	if(pressed || close_pressed) return;
+
+	if(pos.x > bar_height)
+	{
+		SetStyleColor("background-color", GetStyleColor("bg-highlight-color"));
+		close_highlighted = false;
+	}
+	else
+	{
+		SetStyleColor("background-color", GetStyleColor("bg-default-color"));
+		close_highlighted = true;
+	}
+
+	Update();
+}
+
+void UpdateButtonBar::OnMouseLeave()
+{
+	if(!pressed)
+	{
+		SetStyleColor("background-color", GetStyleColor("bg-default-color"));
+	}
+
+	close_highlighted = false;
+
+	Update();
+}
+
+bool UpdateButtonBar::OnMouseDown(const Point& pos, InputKey key)
+{
+	if(key != InputKey::LeftMouse || pressed || close_pressed) return false;
+
+	SetPointerCapture();
+
+	if(pos.x > bar_height)
+	{
+		SetStyleColor("background-color", GetStyleColor("bg-press-color"));
+		pressed = true;
+	}
+	else
+	{
+		close_pressed = true;
+	}
+
+	Update();
+
+	return false;
+}
+
+bool UpdateButtonBar::OnMouseUp(const Point& pos, InputKey key)
+{
+	if(key != InputKey::LeftMouse) return false;
+
+	ReleasePointerCapture();
+
+	if(pos.y > 0 && pos.y < bar_height && pos.x < GetWidth())
+	{
+		if(pos.x > bar_height)
+		{
+			if(pressed)
+			{
+				//TODO
+			}
+
+			SetStyleColor("background-color", GetStyleColor("bg-highlight-color"));
+		}
+		else if(pos.x > 0 && pos.x < bar_height)
+		{
+			if(close_pressed)
+			{
+				//TODO
+				Hide();
+				//GetLauncher()->UpdateSize();
+			}
+
+			SetStyleColor("background-color", GetStyleColor("bg-default-color"));
+		}
+	}
+	else
+	{
+		SetStyleColor("background-color", GetStyleColor("bg-default-color"));
+	}
+
+	pressed = false;
+	close_pressed = false;
+
+	Update();
+
+	return false;
 }
 
 double UpdateButtonBar::GetPreferredHeight() const
 {
-	return 20.0 + UpdateButton->GetPreferredHeight();
-}
-
-void UpdateButtonBar::OnGeometryChanged()
-{
-	UpdateButton->SetFrameGeometry(20.0, 0.0, 120.0, UpdateButton->GetPreferredHeight());
+	return bar_height;
 }
 
 void UpdateButtonBar::OnUpdateButtonClicked()
@@ -56,6 +165,7 @@ LauncherWindow* UpdateButtonBar::GetLauncher() const
 
 void UpdateButtonBar::CheckForUpdate(bool autoupdate)
 {
+	Show();
 	/*
 	if (GetReleaseData())
 	{

src/widgets/updatebuttonbar.h
@@ -22,19 +22,34 @@ class PushButton;
 
 class UpdateButtonBar : public Widget
 {
-  public:
-	UpdateButtonBar(LauncherWindow *parent);
-	void UpdateLanguage();
+	public:
+		UpdateButtonBar(LauncherWindow *parent);
+		void UpdateLanguage();
 
-	double GetPreferredHeight() const;
+		double GetPreferredHeight() const;
 
-	void CheckForUpdate(bool autoupdate);
+		void CheckForUpdate(bool autoupdate);
 
-  private:
-	void OnGeometryChanged() override;
-	void OnUpdateButtonClicked();
+		int GetMargin() { return 4; }
 
-	LauncherWindow *GetLauncher() const;
+	protected:
+		void OnPaint(Canvas* canvas) override;
+		void OnMouseMove(const Point& pos) override;
+		void OnMouseLeave() override;
+		bool OnMouseDown(const Point& pos, InputKey key) override;
+		bool OnMouseUp(const Point& pos, InputKey key) override;
+		void OnUpdateButtonClicked();
 
-	PushButton *UpdateButton = nullptr;
+		LauncherWindow *GetLauncher() const;
+
+		const char * text = "";
+
+		bool close_highlighted = false;
+		bool pressed = false;
+		bool pressed_hover = false;
+		bool close_pressed = false;
+		bool close_pressed_hover = false;
+
+		std::shared_ptr<Image> arrow;
+		std::shared_ptr<Image> close;
 };

commit f551d1294dcf3ec3c35ad38328fbff7fdf43faea

Fix regression introduced in efd61ac, breaking light mode

src/widgets/launcherbanner.cpp
@@ -107,7 +107,7 @@ std::vector<Color::Color> getColors(FName id)
 LauncherBanner::LauncherBanner(Widget* parent, FName colors, float mix) : Widget(parent)
 {
 	bool useColors = colors != "";
-	auto bg = Colorf(0.0f, 0.0f, 0.0f, 0.0f);
+	auto bg = Theme::getHeader(COLOR_BACKGROUND);
 	if (useColors)
 	{
 		auto base = Color::rgb(0xFFFFFF);